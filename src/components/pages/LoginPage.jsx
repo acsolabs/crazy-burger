8d647e8ab5ed11523adf0fe1eeb11dede4e0897d
acsolabs <sofian.achbabi@gmail.com>
2026-04-06 08:51:02 +0200

    ✨ F01 : LOGIN > Add title & Form

--- src/components/pages/LoginPage.jsx
+++ src/components/pages/LoginPage.jsx
@@ -1,5 +1,36 @@
+import { useState } from "react";
+
 function LoginPage() {
-  return <div>LoginPage</div>;
+  //state
+  const [inputValue, setInputValue] = useState("");
+  //comportement
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert(`Bonjour ${inputValue}`);
+    setInputValue("");
+  };
+
+  const handleChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  //render
+  return (
+    <div>
+      <h1>Bienvenue chez nous !</h1>
+      <br />
+      <h2>Connectez-vous</h2>
+      <form action="submit" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Entrez votre prénom"
+          onChange={handleChange}
+          required
+        />
+        <button>Accéder à votre espace</button>
+      </form>
+    </div>
+  );
 }
 
 export default LoginPage;
