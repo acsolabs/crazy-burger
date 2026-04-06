d22380a98048bfe99e9b573cba49e41a37427aa0
acsolabs <sofian.achbabi@gmail.com>
2026-04-06 08:49:02 +0200

    🔥 F01 : Delete Form, Header components

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,6 @@
-import { useState } from "react";
-import Form from "./components/Form";
-import Header from "./components/Header";
+import LoginPage from "./components/pages/LoginPage";
 
 const App = () => {
-  const [name, setName] = useState("");
-
-  const handleChange = (e) => {
-    setName(e.target.value);
-  };
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    alert(`Bonjour ${name}`);
-    setName("");
-  };
-
-  return (
-    <>
-      <Header title="Bienvenue chez nous !" />
-      <Form
-        title2="Connectez-vous"
-        placeholder="Entrez votre prénom..."
-        btnValue="Accédez à votre espace"
-        handleSubmit={handleSubmit}
-        handleChange={handleChange}
-        name={name}
-      />
-    </>
-  );
+  return <LoginPage />;
 };
 export default App;
